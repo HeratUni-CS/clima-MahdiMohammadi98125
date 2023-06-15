a3b13ea2be375c5cfb67f1cb0c82caadbf4d4629
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2023-06-15 09:36:32 +0430

    location_screen.dart updated

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -75,3 +75,25 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
+class IconButtonWidget extends StatelessWidget {
+  const IconButtonWidget({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+  });
+
+  final Function() onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(
+        icon,
+        size: 40.0,
+      ),
+    );
+  }
+}
